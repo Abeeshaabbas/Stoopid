d518037b1d48c22b5c2a19a09b4552e43d9b7cf0
Abeeshaabbas <zainabbasabidi@gmail.com>
2024-01-06 18:35:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
-// Node Sevrer which will handle socket.io connections
-
+// Node Server which will handle socket.io connections
 const express = require('express');
-const app = express();
+const http = require('http');
+const socketIO = require('socket.io');
 const path = require('path');
+
+const app = express();
+const server = http.Server(app);
+const io = socketIO(server);
+
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "node_modules/socket.io/client-dist")));
 console.log(__dirname);
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"))
-})
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
 app.get('/stoopid', (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"))
-})
-app.listen(process.env.PORT, "0.0.0.0", () => {
-    console.log("ho");
-})
-const io = require("socket.io")(8000);
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+server.listen(process.env.PORT, "0.0.0.0", () => {
+    console.log("Server running on port "+process.env.PORT);
+});
+
 const users = {};
 
 io.on('connection', socket => {
-    socket.on('new-user-joined', name=> {
-        console.log("User Joined:",name)
+    socket.on('new-user-joined', name => {
+        console.log("User Joined:", name);
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name);
-    })
-    socket.on('send', message=>{
-        socket.broadcast.emit('receive', {message: message, name: users[socket.id]});
-    })
+    });
+
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
+    });
 
     socket.on('disconnect', message => {
         socket.broadcast.emit('leave', users[socket.id]);
         delete users[socket.id];
-    })
-})
+    });
+});
