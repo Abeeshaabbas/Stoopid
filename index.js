e97601401e95f6da08980de7817c07d555f2b317
Abeeshaabbas <zainabbasabidi@gmail.com>
2024-01-10 00:47:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Node Server which will handle socket.io connections
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
@@ -20,26 +19,26 @@ app.get('/stoopid', (req, res) => {
 });
 
 server.listen(process.env.PORT, "0.0.0.0", () => {
-    console.log("Server running on port "+process.env.PORT);
+    console.log("Server running on port " + process.env.PORT);
 });
 
 const users = {};
 
 io.on('connection', socket => {
 
-
-    socket.on('new-user-joined', name => {
+    socket.on('new-user-joined', ({ name, room }) => {
         console.log("User Joined:", name);
         users[socket.id] = name;
-        socket.broadcast.emit('user-joined', name);
+        socket.join(room);
+        socket.to(room).broadcast.emit('user-joined', name);
     });
 
     socket.on('send', message => {
-        socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
+        socket.to(users[socket.id]).broadcast.emit('receive', { message: message, name: users[socket.id] });
     });
 
     socket.on('disconnect', message => {
-        socket.broadcast.emit('leave', users[socket.id]);
+        socket.to(users[socket.id]).broadcast.emit('leave', users[socket.id]);
         delete users[socket.id];
     });
 });
