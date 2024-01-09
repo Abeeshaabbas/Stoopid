e91dc619437a6609a3469f45fe7bfe089023f32c
Abeeshaabbas <zainabbasabidi@gmail.com>
2024-01-10 01:20:53 +0530

    Update client.js

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ const socket = io('https://stoopid-website.up.railway.app', { transports: ['webs
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
 const messageContainer = document.querySelector(".container");
-
+let currentUser;
 const append = (message, position) => {
     const messageElement = document.createElement('div');
     messageElement.innerText = message;
@@ -32,7 +32,7 @@ let n = prompt("Enter name: ");
 if (n.toString().toLowerCase().includes("ashal") || n.toString().toLowerCase().includes("waliya")) {
     let code = prompt("Enter code");
     if (code == null) window.close();
-    
+    // both users unable to join the room sudoclass fix
     fetch('/config').then(response => response.json()).then(data => {
         if ((code.toLowerCase() === data.secret1) || (code.toLowerCase() === data.secret2)) {
         names = (code.toLowerCase() === data.secret1) ? "Waliya" : "Ashal";
@@ -58,4 +58,8 @@ socket.on('receive', (data) => {
 
 socket.on('leave', name => {
     append(`${name} has left the chat`, 'left');
+});
+
+socket.on('new-user-joined', ({ name }) => {
+    currentUser = name;
 });
