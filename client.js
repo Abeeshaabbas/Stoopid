162273ca73b10705f3c8324a02507489fe81f0ce
Abeeshaabbas <zainabbasabidi@gmail.com>
2024-01-10 00:49:59 +0530

    Update client.js

--- client.js
+++ client.js
@@ -1,9 +1,8 @@
-const socket = io('https://stoopid-website.up.railway.app', { transports : ['websocket'] });
+const socket = io('https://stoopid-website.up.railway.app', { transports: ['websocket'] });
 
 const form = document.getElementById('send-container');
-const messageInpt = document.getElementById('messageInp');
+const messageInput = document.getElementById('messageInp');
 const messageContainer = document.querySelector(".container");
-
 
 const append = (message, position) => {
     const messageElement = document.createElement('div');
@@ -12,45 +11,48 @@ const append = (message, position) => {
     messageElement.classList.add(position);
     messageContainer.append(messageElement);
 }
+
 function scroll() {
-  let chatCont = document.querySelector(".container");
-  chatCont.scrollTop = chatCont.scrollHeight;
+    let chatContainer = document.querySelector(".container");
+    chatContainer.scrollTop = chatContainer.scrollHeight;
 }
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const msg = messageInpt.value;
+    const msg = messageInput.value;
     append(`${msg}`, 'right');
     scroll();
     socket.emit('send', msg);
-    messageInpt.value = ""
-})
+    messageInput.value = "";
+});
+
 let names = "";
 let n = prompt("Enter name: ");
-if(n.toString().toLowerCase().includes("ashal") || n.toString().toLowerCase().includes("waliya")) {
+
+if (n.toString().toLowerCase().includes("ashal") || n.toString().toLowerCase().includes("waliya")) {
     let code = prompt("Enter code");
-if(code==null) window.close();
+    if (code == null) window.close();
 
-if(parseInt(code) === 73 || code.toLowerCase() === "dumbo") {
-    names = "Waliya";
-} else if(parseInt(code) === 143 || code.toLowerCase() === "wfa") {
-    names = "Ashal";
-}  else {
-    names = "Unauthorised user";
+    if ((code.toLowerCase() === process.env.SECRET1) || (code.toLowerCase() === process.env.SECRET2)) {
+        names = (code.toLowerCase() === process.env.SECRET1) ? "Waliya" : "Ashal";
+        socket.emit('new-user-joined', { name: names, room: "sudoclass" });
+    } else {
+        names = "Unauthorised user";
+    }
+} else {
+    names = n;
+    socket.emit('new-user-joined', { name: names, room: "unknown" });
 }
-} else names = n;
 
-
-socket.emit('new-user-joined', names);
-
-socket.on('user-joined', name =>{
+socket.on('user-joined', name => {
     append(`${name} joined the chat`, 'right');
-})
+});
 
 socket.on('receive', (data) => {
     append(`${data.name}: ${data.message}`, 'left');
     scroll();
-})
+});
 
 socket.on('leave', name => {
-    append(`${name} has left the chat`, 'left')
-})
+    append(`${name} has left the chat`, 'left');
+});
